97b7b35e2e1b88ab9ba723cf3a5856b44fbc4920
monicamcg79 <44347528+monicamcg79@users.noreply.github.com>
2024-12-04 20:45:27 +0000

    attempt at pt2

--- src/main/scala/AOC.scala
+++ src/main/scala/AOC.scala
@@ -5,8 +5,17 @@ object AOC extends App {
   val source = Source.fromFile("input.txt")
   val data = source.mkString
   val regex: Regex = """mul\((\d{1,3}),(\d{1,3})\)""".r
-  val matchingMuls = regex.findAllIn(data).toList
-  val total = matchingMuls.map {
+  val regexDo = """do\(\).*do\(\)""".r
+  val regexDont = """don't\(\).*""".r
+
+//  val check = println(regexDo.findAllIn(data).toList)
+  val betweenDos = regexDo.findAllIn(data).mkString
+  println(betweenDos)
+  val removedDont = regexDont.findAllIn(betweenDos).mkString
+  println(removedDont)
+  val muls = regex.findAllIn(betweenDos).toList
+//  println(muls)
+  val total = muls.map {
     str =>
       regex.unapplySeq(str) match {
         case Some(a :: b :: Nil) => a.toInt * b.toInt
